0bb4f507b3c0de0c0dd87c0a71dec0ab37589a98
NicodeSS <RD_Nicode@outlook.com>
2020-04-07 09:23:45 +0800

    fix: 4th class start time

--- CourseExporter.js
+++ CourseExporter.js
@@ -467,7 +467,7 @@ function addClass(icsObj, title, location, day, section, duration) {
       [8, 0, 9, 35],
       [9, 55, 11, 30],
       [13, 30, 15, 5],
-      [15, 25, 16, 55],
+      [15, 20, 16, 55],
       [17, 10, 18, 45],
       [19, 30, 21, 5]
     ];
